bb2966cc99c0952adea9b5b1616c01b5a940d372
Ben <ben@zfben.com>
2019-11-05 16:45:26 +0800

    add http benchmark

--- benchmarks/server/server.sh
+++ benchmarks/server/server.sh
@@ -8,8 +8,25 @@ sleep 2
 
 curl -s http://localhost:3000/ > /dev/null
 
-echo 'FaasJS raw:'
+echo 'FaasJS:'
 wrk 'http://localhost:3000/' \
+  -d 3 \
+  -c 50 \
+  -t 8 \
+  | grep 'Requests/sec'
+
+kill $pid
+
+echo
+FaasLog=warn node node_modules/.bin/faas server -r server/raw -c &
+pid=$!
+
+sleep 2
+
+curl -s http://localhost:3000/http > /dev/null
+
+echo 'FaasJS with http plugin:'
+wrk 'http://localhost:3000/http' \
   -d 3 \
   -c 50 \
   -t 8 \
