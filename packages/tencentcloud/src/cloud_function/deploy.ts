c6d68fd67193d23060834665675e2e49904e227e
Ben <ben@zfben.com>
2020-01-25 19:29:26 +0800

    update tencentcloud function log

--- packages/tencentcloud/src/cloud_function/deploy.ts
+++ packages/tencentcloud/src/cloud_function/deploy.ts
@@ -16,13 +16,14 @@ const defaults = {
 };
 
 export default async function deployCloudFunction (this: Tencentcloud, data: DeployData, origin: any) {
-  this.logger.info('开始发布云函数');
+  this.logger.info('[0/12] 开始发布云函数');
 
   if (!this.config || !this.config.secretId || !this.config.secretKey) throw Error('Missing secretId or secretKey!');
 
   const config = deepMerge(origin);
 
-  this.logger.debug('参数名适配');
+  this.logger.info('[1/12] 生成配置项');
+  // 补全默认参数
   if (config.config.name) {
     config.config.FunctionName = config.config.name;
     delete config.config.name;
@@ -38,7 +39,6 @@ export default async function deployCloudFunction (this: Tencentcloud, data: Dep
     delete config.config.timeout;
   }
 
-  this.logger.debug('合并配置项');
   config.config = deepMerge(defaults, config.config, {
     // 基本参数
     Region: config.provider.config.region,
@@ -71,11 +71,11 @@ export default async function deployCloudFunction (this: Tencentcloud, data: Dep
     CosObjectName: config.config.FunctionName + '/' + data.version + '.zip'
   });
 
-  this.logger.debug('完成参数处理 %o', config);
+  this.logger.debug('[1/12] 完成配置项 %o', config);
 
-  this.logger.info('开始构建代码包');
+  this.logger.info('[2/12] 生成代码包');
 
-  this.logger.debug('生成 index.js');
+  this.logger.debug('[2.1/12] 生成 index.js');
   await loadTs(config.config.filename, {
     output: {
       file: config.config.tmp + '/index.js',
@@ -95,7 +95,7 @@ module.exports = main.export();`
     }
   });
 
-  this.logger.debug('生成 package.json');
+  this.logger.debug('[2.2/12] 生成 package.json');
   const packageJSON = {
     dependencies: config.config.dependencies,
     private: true
@@ -122,29 +122,29 @@ module.exports = main.export();`
   writeFileSync(join(config.config.tmp, 'package.json'), JSON.stringify(packageJSON));
   this.logger.debug('%o', packageJSON);
 
-  this.logger.debug('安装 npm 包');
+  this.logger.debug('[2.3/12] 生成 node_modules');
   execSync('yarn --cwd ' + config.config.tmp + ' install --production');
 
-  this.logger.debug('打包 zip 文件');
+  this.logger.info('[3/12] 打包代码包: %s', config.config.tmp);
   execSync(`cd ${config.config.tmp} && zip -r deploy.zip *`);
 
-  this.logger.info('构建完成 %sdeploy.zip', config.config.tmp);
-
-  this.logger.debug('检查 Bucket 是否存在 %s', config.config.Bucket);
+  this.logger.info('[4/12] 创建 Cos Bucket: %s', config.config.Bucket);
+  this.logger.debug('[4.1/12] 检查 Cos Bucket 状态');
   try {
     await checkBucket.call(this, {
       Bucket: config.config.Bucket,
       Region: config.config.Region
     });
+    this.logger.debug('[4.2/12] Cos Bucket 已存在，跳过');
   } catch (error) {
-    this.logger.info('Bucket 不存在，创建 Bucket %s', config.config.Bucket);
+    this.logger.debug('[4.2/12] 创建 Cos Bucket');
     await createBucket.call(this, {
       Bucket: config.config.Bucket,
       Region: config.config.Region
     });
   }
 
-  this.logger.info('上传代码包');
+  this.logger.info('[5/12] 上传代码包到 Cos Bucket: %s', config.config.FilePath);
   await upload.call(this, {
     Bucket: config.config.Bucket,
     FilePath: config.config.FilePath,
@@ -152,31 +152,37 @@ module.exports = main.export();`
     Region: config.config.Region,
   });
 
-  this.logger.debug('检查命名空间是否存在 %s', config.config.Namespace);
+  this.logger.info('[6/12] 创建命名空间: %s', config.config.Namespace);
+  this.logger.debug('[6.1/12] 检查命名空间状态');
   const namespaceList = await scf.call(this, {
     Action: 'ListNamespaces'
   });
   if (!namespaceList.Namespaces.find(function (n: any) {
     return n.Name === config.config.Namespace;
   })) {
-    this.logger.info('命名空间不存在，创建命名空间');
+    this.logger.debug('[6.2/12] 创建命名空间');
     await scf.call(this, {
       Action: 'CreateNamespace',
       Namespace: config.config.Namespace
     });
+  } else {
+    this.logger.debug('[6.2/12] 命名空间已存在，跳过');
   }
+
+
+  this.logger.info('[7/12] 创建/更新云函数: %s', config.config.FunctionName);
 
   let scfInfo;
 
   try {
-    this.logger.debug('检查云函数是否已存在 %s', config.config.FunctionName);
+    this.logger.debug('[7.1/12] 检查云函数是否已存在');
     scfInfo = await scf.call(this, {
       Action: 'GetFunction',
       FunctionName: config.config.FunctionName,
       Namespace: config.config.Namespace,
     });
 
-    this.logger.info('更新云函数代码');
+    this.logger.debug('[7.2/12] 更新云函数代码');
     await scf.call(this, {
       Action: 'UpdateFunctionCode',
       CosBucketName: 'scf',
@@ -187,8 +193,19 @@ module.exports = main.export();`
       Namespace: config.config.Namespace,
     });
 
+    let status = null;
+    while (status !== 'Active') {
+      this.logger.debug('[7.3/12] 等待云函数代码更新完成');
+
+      status = await scf.call(this, {
+        Action: 'GetFunction',
+        FunctionName: config.config.FunctionName,
+        Namespace: config.config.Namespace
+      }).then(res => res.Status);
+    }
+
     if (scfInfo.MemorySize !== config.config.MemorySize || scfInfo.Timeout !== config.config.Timeout) {
-      this.logger.info('更新云函数设置');
+      this.logger.debug('[7.2/12] 更新云函数配置');
       await scf.call(this, {
         Action: 'UpdateFunctionConfiguration',
         Environment: config.config.Environment,
@@ -198,12 +215,23 @@ module.exports = main.export();`
         VpcConfig: config.config.VpcConfig,
         Namespace: config.config.Namespace,
       });
+
+      let status = null;
+      while (status !== 'Active') {
+        this.logger.debug('[7.3/12] 等待云函数配置更新完成');
+
+        status = await scf.call(this, {
+          Action: 'GetFunction',
+          FunctionName: config.config.FunctionName,
+          Namespace: config.config.Namespace
+        }).then(res => res.Status);
+      }
     } else {
-      this.logger.info('云函数设置未变更，跳过更新');
+      this.logger.debug('[7.2/12] 云函数设置未变更，跳过');
     }
   } catch (error) {
     if (error.Code === 'ResourceNotFound.FunctionName') {
-      this.logger.info('创建云函数');
+      this.logger.debug('[7.2/12] 创建云函数');
       await scf.call(this, {
         Action: 'CreateFunction',
         Code: {
@@ -220,23 +248,23 @@ module.exports = main.export();`
         Timeout: config.config.Timeout,
         VpcConfig: config.config.VpcConfig,
       });
+
+      let status = null;
+      while (status !== 'Active') {
+        this.logger.info('[7.3/12] 等待云函数代码更新完成');
+
+        status = await scf.call(this, {
+          Action: 'GetFunction',
+          FunctionName: config.config.FunctionName,
+          Namespace: config.config.Namespace
+        }).then(res => res.Status);
+      }
     } else {
       throw error;
     }
   }
 
-  let status = null;
-  while (status !== 'Active') {
-    this.logger.info('等待云函数代码更新完成');
-
-    status = await scf.call(this, {
-      Action: 'GetFunction',
-      FunctionName: config.config.FunctionName,
-      Namespace: config.config.Namespace
-    }).then(res => res.Status);
-  }
-
-  this.logger.info('发布云函数版本');
+  this.logger.info('[8/12] 发布版本: %i', Number(scfInfo.FunctionVersion) + 1);
 
   const version = await scf.call(this, {
     Action: 'PublishVersion',
@@ -247,8 +275,10 @@ module.exports = main.export();`
   // eslint-disable-next-line require-atomic-updates
   config.config.FunctionVersion = version.FunctionVersion;
 
+  this.logger.info('[9/12] 创建/更新别名: %s', config.config.Namespace);
+
   try {
-    this.logger.debug('检查云函数别名是否已存在 %s', config.config.Namespace);
+    this.logger.debug('[9.1/12] 检查别名状态');
     await scf.call(this, {
       Action: 'GetAlias',
       Name: config.config.Namespace,
@@ -256,6 +286,7 @@ module.exports = main.export();`
       Namespace: config.config.Namespace
     });
 
+    this.logger.info('[9.2/12] 更新别名');
     await scf.call(this, {
       Action: 'UpdateAlias',
       Name: config.config.Namespace,
@@ -265,7 +296,7 @@ module.exports = main.export();`
     });
   } catch (error) {
     if (error.Code === 'ResourceNotFound.Alias') {
-      this.logger.info('发布云函数别名');
+      this.logger.info('[9.2/12] 创建别名');
       await scf.call(this, {
         Action: 'CreateAlias',
         Name: config.config.Namespace,
@@ -278,20 +309,12 @@ module.exports = main.export();`
     }
   }
 
-  this.logger.info('云函数发布完成 %s/%s@%s', config.config.Namespace, config.config.FunctionName, config.config.FunctionVersion);
-
-  this.logger.debug('删除代码包');
-  await remove.call(this, {
-    Bucket: config.config.Bucket,
-    Key: config.config.CosObjectName,
-    Region: config.config.Region,
-  });
-
   if (config.config.triggers) {
-    this.logger.info('检查并删除旧触发器');
+    this.logger.info('[10/12] 创建/更新触发器: %o', config.config.triggers);
     const prevVersion = Number(config.config.FunctionVersion) - 1;
     if (scfInfo && scfInfo.Triggers.length) {
       for (const trigger of scfInfo.Triggers) {
+        this.logger.debug('[10.1/12] 删除旧触发器: %s', trigger.TriggerName);
         await scf.call(this, {
           Action: 'DeleteTrigger',
           FunctionName: config.config.FunctionName,
@@ -311,6 +334,7 @@ module.exports = main.export();`
       });
       if (scfInfo.Triggers.length) {
         for (const trigger of scfInfo.Triggers) {
+          this.logger.debug('[10.1/12] 删除旧触发器: %s', trigger.TriggerName);
           await scf.call(this, {
             Action: 'DeleteTrigger',
             FunctionName: config.config.FunctionName,
@@ -324,7 +348,7 @@ module.exports = main.export();`
     }
 
     for (const trigger of config.config.triggers) {
-      this.logger.info('发布触发器 %o', trigger);
+      this.logger.debug('[10.2/12] 创建触发器 %o', trigger);
       await scf.call(this, {
         Action: 'CreateTrigger',
         FunctionName: config.config.FunctionName,
@@ -336,10 +360,19 @@ module.exports = main.export();`
         Enable: 'OPEN'
       });
     }
-
-    this.logger.info('触发器发布完成 %o', config.config.triggers);
-
-    this.logger.info('删除临时文件');
-    execSync(`rm -rf ${config.config.tmp}`);
   }
+
+  this.logger.info('[11/12] 清理文件');
+
+  this.logger.debug('[11.1/12] 清理 Cos Bucket: %s', config.config.CosObjectName);
+  await remove.call(this, {
+    Bucket: config.config.Bucket,
+    Key: config.config.CosObjectName,
+    Region: config.config.Region,
+  });
+  
+  this.logger.debug('[11.2/12] 清理本地文件: %s', config.config.tmp);
+  execSync(`rm -rf ${config.config.tmp}`);
+
+  this.logger.info('[12/12] 完成发布 %s/%s@%s', config.config.Namespace, config.config.FunctionName, config.config.FunctionVersion);
 }
