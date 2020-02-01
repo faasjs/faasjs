037940c8475f7d78873103f7e5ea1add61e0b5b0
Ben <ben@zfben.com>
2020-02-01 18:10:28 +0800

    update script

---
+++ tag.js
@@ -0,0 +1,12 @@
+const globSync = require('glob').sync;
+const execSync = require('child_process').execSync;
+
+for (const pkg of globSync('packages/*/package.json').map(f => require('./' + f))) {
+  try {
+    const cmd = `npm dist-tag add ${pkg.name}@${pkg.version} latest`;
+    console.log(cmd);
+    execSync(cmd, { stdio: 'inherit' })
+  } catch (error) {
+    
+  }
+}
