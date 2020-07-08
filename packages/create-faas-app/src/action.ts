0116c5dff4e729f081c029bb376ec78c576fcb1b
onichandame <zxinmyth@gmail.com>
2020-07-08 06:26:11 +0000

    lint

--- packages/create-faas-app/src/action.ts
+++ packages/create-faas-app/src/action.ts
@@ -1,39 +1,39 @@
-import {Command} from 'commander';
-import {prompt} from 'enquirer';
-import {mkdirSync, writeFileSync, existsSync} from 'fs';
-import {join} from 'path';
-import {execSync} from 'child_process';
+import { Command } from 'commander';
+import { prompt } from 'enquirer';
+import { mkdirSync, writeFileSync, existsSync } from 'fs';
+import { join } from 'path';
+import { execSync } from 'child_process';
 
 const Provider = ['tencentcloud', null];
 const Region = ['ap-beijing', 'ap-shanghai', 'ap-guangzhou', 'ap-hongkong'];
 
 const Validator = {
-  name(input: string) {
+  name (input: string) {
     const match = /^[a-z0-9-_]+$/i.test(input) ? true : 'Must be a-z, 0-9 or -_';
     if (match !== true) return match;
-    if (existsSync(input)) {
+    if (existsSync(input)) 
       return `${input} folder exists, please try another name`;
-    }
+    
     return true;
   },
-  provider(input: string | null) {
+  provider (input: string | null) {
     return Provider.includes(input) ? true : 'Unknow provider';
   },
-  region(input: string) {
+  region (input: string) {
     return Region.includes(input) ? true : 'Unknown region';
   },
-  appId(input: string) {
+  appId (input: string) {
     return /^[0-9]+$/.test(input) ? true : 'Wrong format';
   },
-  secretId(input: string) {
+  secretId (input: string) {
     return /^[a-zA-Z0-9]+$/.test(input) ? true : 'Wrong format';
   },
-  secretKey(input: string) {
+  secretKey (input: string) {
     return /^[a-zA-Z0-9]+$/.test(input) ? true : 'Wrong format';
   }
 };
 
-export async function action(options?: {
+export async function action (options?: {
   name?: string;
   region?: string;
   appId?: string;
@@ -51,14 +51,14 @@ export async function action(options?: {
     example?: boolean;
   } = Object.assign(options, {});
 
-  if (!options.name || Validator.name(options.name) !== true) {
+  if (!options.name || Validator.name(options.name) !== true) 
     answers.name = await prompt({
       type: 'input',
       name: 'value',
       message: 'Project name',
       validate: Validator.name
     }).then((res: {value: string}) => res.value);
-  }
+  
 
   answers.provider = await prompt({
     type: 'select',
@@ -77,7 +77,7 @@ export async function action(options?: {
   }).then((res: {value: string}) => res.value);
 
   if (answers.provider === 'tencentcloud') {
-    if (!answers.region || Validator.region(answers.region) !== true) {
+    if (!answers.region || Validator.region(answers.region) !== true) 
       answers.region = await prompt({
         type: 'select',
         name: 'value',
@@ -85,44 +85,44 @@ export async function action(options?: {
         choices: Region.concat([]), // choices 会修改 Region 对象，因此克隆一份
         validate: Validator.region
       }).then((res: {value: string}) => res.value);
-    }
+    
 
-    if (!answers.appId || Validator.appId(answers.appId) !== true) {
+    if (!answers.appId || Validator.appId(answers.appId) !== true) 
       answers.appId = await prompt({
         type: 'input',
         name: 'value',
         message: 'appId (from https://console.cloud.tencent.com/developer)',
         validate: Validator.appId
       }).then((res: {value: string}) => res.value);
-    }
+    
 
-    if (!answers.secretId || Validator.secretId(answers.secretId) !== true) {
+    if (!answers.secretId || Validator.secretId(answers.secretId) !== true) 
       answers.secretId = await prompt({
         type: 'input',
         name: 'value',
         message: 'secretId (from https://console.cloud.tencent.com/cam/capi)',
         validate: Validator.secretId
       }).then((res: {value: string}) => res.value);
-    }
+    
 
-    if (!answers.secretKey || Validator.secretKey(answers.secretKey) !== true) {
+    if (!answers.secretKey || Validator.secretKey(answers.secretKey) !== true) 
       answers.secretKey = await prompt({
         type: 'input',
         name: 'value',
         message: 'secretKey (from https://console.cloud.tencent.com/cam/capi)',
         validate: Validator.secretKey
       }).then((res: {value: string}) => res.value);
-    }
+    
   }
 
-  if (answers.example !== false) {
+  if (answers.example !== false) 
     answers.example = await prompt({
       type: 'confirm',
       name: 'value',
       message: 'Add example files',
       initial: true
     }).then((res: {value: boolean}) => res.value);
-  }
+  
 
   mkdirSync(answers.name);
   writeFileSync(join(answers.name, 'faas.yaml'),
@@ -201,7 +201,7 @@ tmp/
   "files.trimFinalNewlines": true
 }`);
 
-  execSync(`yarn --cwd ${answers.name} install`, {stdio: 'inherit'});
+  execSync(`yarn --cwd ${answers.name} install`, { stdio: 'inherit' });
 
   if (answers.example) {
     writeFileSync(join(answers.name, 'index.func.ts'),
@@ -229,7 +229,7 @@ describe('hello', function () {
   });
 });`);
 
-    execSync(`yarn --cwd ${answers.name} jest`, {stdio: 'inherit'});
+    execSync(`yarn --cwd ${answers.name} jest`, { stdio: 'inherit' });
   }
 }
 
