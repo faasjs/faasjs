b206d616ce11cb58a73598cf7f2ae307c969dd48
Ben <ben@zfben.com>
2020-10-15 21:00:32 +0800

    update case

--- packages/test/src/__tests__/http.test.ts
+++ packages/test/src/__tests__/http.test.ts
@@ -8,11 +8,19 @@ test('http', async function () {
   expect(res.body).toEqual('{"data":true}');
 });
 
-test('JSONhandler', async function () {
+test('JSONhandler data', async function () {
   const func = new FuncWarpper(require.resolve('./funcs/json.func'));
 
   const res = await func.JSONhandler<number>({ key: 1 });
 
   expect(res.body).toEqual('{"data":1}');
   expect(res.data).toEqual(1);
+});
+
+test('JSONhandler error', async function () {
+  const func = new FuncWarpper(require.resolve('./funcs/http-error.func'));
+
+  const res = await func.JSONhandler();
+
+  expect(res.error.message).toEqual('message');
 });
--- packages/test/src/__tests__/initByFunc.test.ts
+++ packages/test/src/__tests__/initByFunc.test.ts
@@ -1,9 +1,11 @@
 import { Func, FuncWarpper } from '../../src/index';
 
-test('init by func', async function () {
-  const func = new Func({ async handler () { return true; } });
-  const warper = new FuncWarpper(func);
-  const res = await warper.handler({}, {});
+describe('init by func', function () {
+  test('200', async function () {
+    const func = new Func({ async handler () { return true; } });
+    const warper = new FuncWarpper(func);
+    const res = await warper.handler({}, {});
 
-  expect(res).toEqual(true);
+    expect(res).toEqual(true);
+  });
 });
