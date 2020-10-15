761d5d2f844db33cfee52c4daec72dcbba4cf12e
Ben <ben@zfben.com>
2020-10-15 21:00:40 +0800

    update case

---
+++ packages/test/src/__tests__/funcs/http-error.func.ts
@@ -0,0 +1,9 @@
+import { Func } from '@faasjs/func';
+import { Http } from '@faasjs/http';
+
+export default new Func({
+  plugins: [new Http()],
+  async handler () {
+    throw Error('message');
+  }
+});
