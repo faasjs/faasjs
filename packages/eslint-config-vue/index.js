d09f21afb568a5b00e8186638e333fc4a244bc4e
Ben <ben@zfben.com>
2020-11-30 19:44:31 +0800

    fix eslint config

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -5,56 +5,8 @@ module.exports = {
       es6: true
     },
     parser: "vue-eslint-parser",
-    parserOptions: {
-      "parser": "@typescript-eslint/parser"
-    },
-    plugins: [
-      "security",
-      "@typescript-eslint"
-    ],
     extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
+      "@faasjs/recommended",
       "plugin:vue/essential"
-    ],
-    rules: {
-      "no-console": "off",
-      "quotes": [
-        "error",
-        "single"
-      ],
-      "keyword-spacing": "error",
-      "space-before-blocks": "error",
-      "space-before-function-paren": "error",
-      "semi": [
-        "error",
-        "always"
-      ],
-      "eol-last": [
-        "error",
-        "always"
-      ],
-      "quote-props": [
-        "error",
-        "as-needed"
-      ],
-      "object-property-newline": [
-        "error"
-      ],
-      "@typescript-eslint/indent": [
-        "error",
-        2
-      ],
-      "camelcase": "off",
-      "@typescript-eslint/camelcase": [
-        "error",
-        {
-          "properties": "never"
-        }
-      ],
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/explicit-member-accessibility": "off"
-    }
+    ]
   };
