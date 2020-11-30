cb77f30acddd399272b56455753959af3337aef6
Ben <ben@zfben.com>
2020-11-30 19:38:40 +0800

    update cases

---
+++ packages/load/src/__tests__/extend.func.ts
@@ -0,0 +1,6 @@
+import deepMerge from '@faasjs/deep_merge';
+import extend from './extended';
+
+deepMerge({});
+
+export default extend;
---
+++ packages/load/src/__tests__/require.func.ts
@@ -0,0 +1,7 @@
+import deepMerge from '@faasjs/deep_merge';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const required = require('./required');
+
+deepMerge({});
+
+export default required;
