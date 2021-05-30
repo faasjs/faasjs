3dac4500956e49643ea7a48a09eaf33620f60d15
Ben <ben@zfben.com>
2021-05-30 19:18:07 +0800

    update case

--- packages/tencentcloud/src/__tests__/cloud_function/update_deploy.test.ts
+++ packages/tencentcloud/src/__tests__/cloud_function/update_deploy.test.ts
@@ -1,6 +1,11 @@
 import { join, sep } from 'path';
 import Tencentcloud from '../..';
 
+jest.mock('child_process', function () {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  return { execSync: function () {} };
+});
+
 jest.mock('cos-nodejs-sdk-v5', () => {
   return class Client {
     headBucket (params, callback) {
