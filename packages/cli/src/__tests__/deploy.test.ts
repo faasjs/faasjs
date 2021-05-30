fc9e5a3c48bb0e8bf634ada55995b031693c18a0
Ben <ben@zfben.com>
2021-05-30 12:57:14 +0800

    update case

--- packages/cli/src/__tests__/deploy.test.ts
+++ packages/cli/src/__tests__/deploy.test.ts
@@ -54,8 +54,7 @@ describe('deploy', function () {
     test('file', async function () {
       await expect(action('testing', [__dirname + '/funcs/a.func.ts'], {
         ar: '0',
-        w: '1',
-        y: '1'
+        w: '1'
       })).rejects.toEqual(Error(__dirname + '/funcs/a.func.ts 自动重试次数已满，结束重试'));
 
       expect(logs).toEqual([]);
@@ -66,52 +65,10 @@ describe('deploy', function () {
     });
 
     test('folder', async function () {
-      await action('testing', [__dirname + '/funcs'], {
+      await expect(action('testing', [__dirname + '/funcs'], {
         ar: '0',
-        y: '1'
-      });
-
-      expect(logs).toEqual([
-        '[testing] 是否要发布以下 2 个云函数？(并行数 2，失败自动重试 0 次)',
-        [
-          __dirname + '/funcs/a.func.ts',
-          __dirname + '/funcs/b.func.ts'
-        ],
-        ''
-      ]);
-      expect(warns).toEqual([]);
-      expect(errors).toEqual([]);
-    });
-  });
-
-  describe('options', function () {
-    test('ar', async function () {
-      await expect(action('testing', [__dirname + '/funcs/a.func.ts'], {
-        ar: '1',
-        w: '1',
-        y: '1'
+        w: '1'
       })).rejects.toEqual(Error(__dirname + '/funcs/a.func.ts 自动重试次数已满，结束重试'));
-
-      expect(logs.length).toEqual(1);
-      expect(logs[0]).toContain('等待 ');
-      expect(warns).toEqual([
-        __dirname + '/funcs/a.func.ts 自动重试（剩余 1 次）'
-      ]);
-      expect(errors).toEqual([
-        Error('Missing secretId or secretKey!'),
-        Error('Missing secretId or secretKey!')
-      ]);
-    });
-
-    test('y', async function () {
-      try {
-        await action('testing', [__dirname + '/funcs'], {
-          ar: '0',
-          w: '1',
-        });
-      } catch (error) {
-        console.error(error);
-      }
 
       expect(logs).toEqual([
         '[testing] 是否要发布以下 2 个云函数？(并行数 1，失败自动重试 0 次)',
@@ -126,6 +83,44 @@ describe('deploy', function () {
       expect(errors).toEqual([
         Error('Missing secretId or secretKey!')
       ]);
+    });
+  });
+
+  describe('options', function () {
+    test('ar', async function () {
+      await expect(action('testing', [__dirname + '/funcs/a.func.ts'], {
+        ar: '1',
+        w: '1'
+      })).rejects.toEqual(Error(__dirname + '/funcs/a.func.ts 自动重试次数已满，结束重试'));
+
+      expect(logs.length).toEqual(1);
+      expect(logs[0]).toContain('等待 ');
+      expect(warns).toEqual([
+        __dirname + '/funcs/a.func.ts 自动重试（剩余 1 次）'
+      ]);
+      expect(errors).toEqual([
+        Error('Missing secretId or secretKey!'),
+        Error('Missing secretId or secretKey!')
+      ]);
+    });
+
+    test('w', async function () {
+      await action('testing', [__dirname + '/funcs'], {
+        ar: '0',
+        w: '3'
+      });
+
+      expect(logs).toEqual([
+        '[testing] 是否要发布以下 2 个云函数？(并行数 2，失败自动重试 0 次)',
+        [
+          __dirname + '/funcs/a.func.ts',
+          __dirname + '/funcs/b.func.ts'
+        ],
+        '',
+        '开始发布'
+      ]);
+      expect(warns).toEqual([]);
+      expect(errors).toEqual([]);
     });
   });
 });
