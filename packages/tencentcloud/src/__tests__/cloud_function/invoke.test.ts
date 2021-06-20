70289d2365cf4c3b57e14591ee893f747791810b
Ben <ben@zfben.com>
2021-06-20 11:01:40 +0800

    add case

--- packages/tencentcloud/src/__tests__/cloud_function/invoke.test.ts
+++ packages/tencentcloud/src/__tests__/cloud_function/invoke.test.ts
@@ -1,4 +1,5 @@
 import Tencentcloud from '../..';
+import { setMock } from '@faasjs/request';
 
 describe('invoke', function () {
   describe('basic', function () {
@@ -78,5 +79,57 @@ describe('invoke', function () {
     } catch (error) {
       expect(error.message).toEqual('wrong');
     }
+  });
+
+  describe('mock network', function () {
+    beforeAll(function () {
+      delete process.env.FaasMode;
+      setMock(null);
+    });
+
+    afterAll(function () {
+      process.env.FaasMode = 'local';
+      setMock(null);
+    });
+
+    test('success', async function () {
+      setMock(async function () {
+        return Promise.resolve({
+          headers: {},
+          body: { Response: { Result: {} } }
+        });
+      });
+
+      const tc = new Tencentcloud({
+        secretId: 'secretId',
+        secretKey: 'secretKey',
+        region: 'region'
+      });
+
+      await expect(tc.invokeSyncCloudFunction('../__tests__/funcs/basic', {
+        event: {},
+        context: {}
+      })).resolves.toEqual({ Result: {} });
+    });
+
+    test('fail', async function () {
+      setMock(async function () {
+        return Promise.resolve({
+          headers: {},
+          body: { Response: { Result: { ErrMsg: 'ErrMsg' } } }
+        });
+      });
+
+      const tc = new Tencentcloud({
+        secretId: 'secretId',
+        secretKey: 'secretKey',
+        region: 'region'
+      });
+
+      await expect(tc.invokeSyncCloudFunction('../__tests__/funcs/basic', {
+        event: {},
+        context: {}
+      })).rejects.toEqual(Error('ErrMsg'));
+    });
   });
 });
