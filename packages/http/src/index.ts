d86c10fd2cec45f689460dc90730d292b614bb23
Ben <ben@zfben.com>
2021-06-21 12:03:21 +0800

    fix http

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -143,8 +143,7 @@ export class Http<TParams = {[key: string]: any }, TCookie = { [key: string]: st
 
     // 引用服务商部署插件
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const Provider = require(config.provider.type).default
-    console.log(config.provider.type, Provider)
+    const Provider = require(config.provider.type)
     const provider = new Provider(config.provider.config)
 
     // 部署网关
