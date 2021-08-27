2fc6bbfe51eab1ca11c6eaaefda33a1487e63307
Ben <ben@zfben.com>
2021-08-27 18:09:59 +0800

    update jest

--- packages/deployer/src/__tests__/basic.test.ts
+++ packages/deployer/src/__tests__/basic.test.ts
@@ -12,12 +12,11 @@ test('basic', async function () {
 
   try {
     await deployer.deploy()
-  // eslint-disable-next-line no-empty
   } catch (error) {
     console.error(error)
   }
 
   const res = execSync(`node -e "const handler = require('${deployer.deployData.tmp}index.js').handler;(async function invoke(){console.log('|'+JSON.stringify(await handler(0))+'|');})(handler);"`, { cwd: deployer.deployData.tmp }).toString()
 
-  expect(res.match(/([^|]+)|$/g)![1]).toEqual('1')
+  expect(res.match(/([^|]+)|$/g)[1]).toEqual('1')
 }, 100000)
