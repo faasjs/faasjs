53b664432454d3dbc3fd3bf202492b13821ed833
Ben <ben@zfben.com>
2021-11-03 12:41:48 +0800

    reduce dup code

--- packages/tencentcloud/src/cloud_function/cos.ts
+++ packages/tencentcloud/src/cloud_function/cos.ts
@@ -3,12 +3,16 @@ import { Provider } from '..'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cosSdk = require('cos-nodejs-sdk-v5')
 
+function cos (tc: Provider) {
+  return new cosSdk({
+    SecretId: tc.config.secretId,
+    SecretKey: tc.config.secretKey
+  })
+}
+
 export async function checkBucket (tc: Provider, params: { [key: string]: any }): Promise<any> {
   return await new Promise((resolve, reject) => {
-    new cosSdk({
-      SecretId: tc.config.secretId,
-      SecretKey: tc.config.secretKey
-    }).headBucket(params, function (err: any, data: any) {
+    cos(tc).headBucket(params, function (err: any, data: any) {
       if (err)
         reject(err)
       resolve(data)
@@ -18,10 +22,7 @@ export async function checkBucket (tc: Provider, params: { [key: string]: any })
 
 export async function createBucket (tc: Provider, params: { [key: string]: any }): Promise<any> {
   return await new Promise((resolve, reject) => {
-    new cosSdk({
-      SecretId: tc.config.secretId,
-      SecretKey: tc.config.secretKey
-    }).putBucket(params, function (err: any, data: any) {
+    cos(tc).putBucket(params, function (err: any, data: any) {
       if (err)
         reject(err)
       resolve(data)
@@ -31,10 +32,7 @@ export async function createBucket (tc: Provider, params: { [key: string]: any }
 
 export async function upload (tc: Provider, params: { [key: string]: any }): Promise<any> {
   return await new Promise((resolve, reject) => {
-    new cosSdk({
-      SecretId: tc.config.secretId,
-      SecretKey: tc.config.secretKey
-    }).sliceUploadFile(params, function (err: any, data: any) {
+    cos(tc).sliceUploadFile(params, function (err: any, data: any) {
       if (err)
         reject(err)
       resolve(data)
@@ -44,10 +42,7 @@ export async function upload (tc: Provider, params: { [key: string]: any }): Pro
 
 export async function remove (tc: Provider, params: { [key: string]: any }): Promise<any> {
   return await new Promise((resolve, reject) => {
-    new cosSdk({
-      SecretId: tc.config.secretId,
-      SecretKey: tc.config.secretKey
-    }).deleteObject(params, function (err: any, data: any) {
+    cos(tc).deleteObject(params, function (err: any, data: any) {
       if (err)
         reject(err)
       resolve(data)
