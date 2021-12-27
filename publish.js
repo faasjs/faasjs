0bd4beb1aed80d5972bd9f47097c64e76789e45b
Ben <benz303bb@gmail.com>
2021-12-27 17:26:32 +0800

    update publish script

--- publish.js
+++ publish.js
@@ -9,28 +9,36 @@ function exec(cmd) {
   execSync(cmd, { stdio: 'inherit' })
 }
 
-for (const path of globSync('packages/*/package.json')) {
-  const pkg = require(__dirname + '/' + path)
-  pkg.version = version
-  if (pkg.peerDependencies) {
-    for (const name of Object.keys(pkg.peerDependencies)) {
-      if (name.startsWith('@faasjs/'))
-        pkg.peerDependencies[name] = '^' + version
+async function publish(path) {
+  return new Promise((res) => {
+    const pkg = require(__dirname + '/' + path)
+    pkg.version = version
+    if (pkg.peerDependencies) {
+      for (const name of Object.keys(pkg.peerDependencies)) {
+        if (name.startsWith('@faasjs/'))
+          pkg.peerDependencies[name] = '^' + version
+      }
     }
-  }
-  if (pkg.devDependencies) {
-    for (const name of Object.keys(pkg.devDependencies)) {
-      if (name.startsWith('@faasjs/'))
-        pkg.devDependencies[name] = '^' + version
+    if (pkg.devDependencies) {
+      for (const name of Object.keys(pkg.devDependencies)) {
+        if (name.startsWith('@faasjs/'))
+          pkg.devDependencies[name] = '^' + version
+      }
     }
-  }
-  writeFileSync(path, JSON.stringify(pkg, null, 2) + '\n')
-  if (pkg.scripts && pkg.scripts.build)
-    exec(`npm run build -w ${path.replace('/package.json', '')}`)
-  exec(`npm publish -w ${path.replace('/package.json', '')} --access public`)
-  exec(`npm dist-tag add ${pkg.name}@${pkg.version} beta`)
+    writeFileSync(path, JSON.stringify(pkg, null, 2) + '\n')
+    if (pkg.scripts && pkg.scripts.build)
+      exec(`npm run build -w ${path.replace('/package.json', '')}`)
+    exec(`npm publish -w ${path.replace('/package.json', '')} --access public`)
+    exec(`npm dist-tag add ${pkg.name}@${pkg.version} beta`)
+    res()
+  })
 }
 
-exec(`git commit -am 'release ${version}'`)
-exec(`git tag ${version}`)
-exec('git push && git push --tags')
+async function publishAll() {
+  await Promise.all(globSync('packages/*/package.json').map(publish))
+  exec(`git commit -am 'release ${version}'`)
+  exec(`git tag ${version}`)
+  exec('git push && git push --tags')
+}
+
+publishAll()
