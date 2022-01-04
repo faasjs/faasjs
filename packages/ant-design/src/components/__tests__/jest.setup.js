b36c0df29a870bba37b8e874382be222a17c6e0f
Ben <benz303bb@gmail.com>
2022-01-04 15:19:53 +0800

    fix jest

--- packages/ant-design/src/components/__tests__/jest.setup.js
+++ packages/ant-design/src/components/__tests__/jest.setup.js
@@ -1,19 +1,19 @@
-import '@testing-library/jest-dom'
-import React from 'react'
+if (typeof window !== 'undefined') {
+  require('@testing-library/jest-dom')
+  global.React = require('react')
 
-global.React = React
-
-// eslint-disable-next-line no-undef
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-})
+  // eslint-disable-next-line no-undef
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  })
+}
