d2a4fd4786e1748e43648ae1e3134e2cf4e0cc3f
Yifeng Sun <15251516870@163.com>
2022-01-28 20:05:43 +0800

    eslint format code

--- packages/http/src/validator.ts
+++ packages/http/src/validator.ts
@@ -1,14 +1,14 @@
-import { Cookie } from "./cookie";
-import { Session } from "./session";
-import { Logger } from "@faasjs/logger";
-import { HttpError } from ".";
+import { Cookie } from './cookie'
+import { Session } from './session'
+import { Logger } from '@faasjs/logger'
+import { HttpError } from '.'
 
 export type ValidatorRuleOptionsType =
-  | "string"
-  | "number"
-  | "boolean"
-  | "object"
-  | "array";
+  | 'string'
+  | 'number'
+  | 'boolean'
+  | 'object'
+  | 'array'
 
 export type ValidatorRuleOptions = {
   type?: ValidatorRuleOptionsType;
@@ -17,10 +17,10 @@ export type ValidatorRuleOptions = {
   default?: any;
   config?: Partial<ValidatorOptions>;
   regexp?: RegExp;
-};
+}
 
 export type ValidatorOptions<Content = Record<string, any>> = {
-  whitelist?: "error" | "ignore";
+  whitelist?: 'error' | 'ignore';
   rules: {
     [k in keyof Content]?: ValidatorRuleOptions;
   };
@@ -32,7 +32,7 @@ export type ValidatorOptions<Content = Record<string, any>> = {
     statusCode?: number;
     message: any;
   } | void;
-};
+}
 
 type Request<
   TParams extends Record<string, any> = any,
@@ -45,7 +45,7 @@ type Request<
   params?: TParams;
   cookie?: Cookie<TCookie, TSession>;
   session?: Session<TSession, TCookie>;
-};
+}
 
 export type BeforeOption<
   TParams extends Record<string, any> = any,
@@ -54,7 +54,7 @@ export type BeforeOption<
 > = (request: Request<TParams, TCookie, TSession>) => Promise<void | {
   statusCode?: number;
   message: string;
-}>;
+}>
 
 export type ValidatorConfig<
   TParams extends Record<string, any> = any,
@@ -65,21 +65,21 @@ export type ValidatorConfig<
   cookie?: ValidatorOptions<TCookie>;
   session?: ValidatorOptions<TSession>;
   before?: BeforeOption;
-};
+}
 
 export class Validator<
   TParams extends Record<string, any> = any,
   TCookie extends Record<string, string> = any,
   TSession extends Record<string, string> = any
 > {
-  public before?: BeforeOption<TParams, TCookie, TSession>;
-  public paramsConfig?: ValidatorOptions<TParams>;
-  public cookieConfig?: ValidatorOptions<TCookie>;
-  public sessionConfig?: ValidatorOptions<TSession>;
-  private request: Request<TParams, TCookie, TSession>;
-  private readonly logger: Logger;
+  public before?: BeforeOption<TParams, TCookie, TSession>
+  public paramsConfig?: ValidatorOptions<TParams>
+  public cookieConfig?: ValidatorOptions<TCookie>
+  public sessionConfig?: ValidatorOptions<TSession>
+  private request: Request<TParams, TCookie, TSession>
+  private readonly logger: Logger
 
-  constructor(
+  constructor (
     config: {
       params?: ValidatorOptions<TParams>;
       cookie?: ValidatorOptions<TCookie>;
@@ -88,54 +88,54 @@ export class Validator<
     },
     logger: Logger
   ) {
-    this.paramsConfig = config.params;
-    this.cookieConfig = config.cookie;
-    this.sessionConfig = config.session;
-    this.before = config.before;
-    this.logger = logger;
+    this.paramsConfig = config.params
+    this.cookieConfig = config.cookie
+    this.sessionConfig = config.session
+    this.before = config.before
+    this.logger = logger
   }
 
-  public async valid(
+  public async valid (
     request: Request<TParams, TCookie, TSession>
   ): Promise<void> {
     if (this.before) {
-      const result = await this.before(request);
+      const result = await this.before(request)
 
-      if (result) throw new HttpError(result);
+      if (result) throw new HttpError(result)
     }
 
-    this.request = request;
+    this.request = request
 
     if (this.paramsConfig && request.params) {
-      this.logger.debug("Valid Params");
-      this.validContent("params", request.params, "", this.paramsConfig);
+      this.logger.debug('Valid Params')
+      this.validContent('params', request.params, '', this.paramsConfig)
     }
 
     if (this.cookieConfig && request.cookie) {
-      this.logger.debug("Valid Cookie");
-      if (request.cookie == null) throw Error("Not found Cookie");
+      this.logger.debug('Valid Cookie')
+      if (request.cookie == null) throw Error('Not found Cookie')
 
       this.validContent(
-        "cookie",
+        'cookie',
         request.cookie.content,
-        "",
+        '',
         this.cookieConfig
-      );
+      )
     }
 
     if (this.sessionConfig && request.session) {
-      this.logger.debug("Valid Session");
-      if (request.session == null) throw Error("Not found Session");
+      this.logger.debug('Valid Session')
+      if (request.session == null) throw Error('Not found Session')
       this.validContent(
-        "session",
+        'session',
         request.session.content,
-        "",
+        '',
         this.sessionConfig
-      );
+      )
     }
   }
 
-  public validContent(
+  public validContent (
     type: string,
     params: {
       [key: string]: any;
@@ -144,135 +144,135 @@ export class Validator<
     config: ValidatorOptions
   ): void {
     if (config.whitelist) {
-      const paramsKeys = Object.keys(params);
-      const rulesKeys = Object.keys(config.rules);
-      const diff = paramsKeys.filter((k) => !rulesKeys.includes(k));
+      const paramsKeys = Object.keys(params)
+      const rulesKeys = Object.keys(config.rules)
+      const diff = paramsKeys.filter((k) => !rulesKeys.includes(k))
       if (diff.length > 0)
-        if (config.whitelist === "error") {
-          const diffKeys = diff.map((k) => `${baseKey}${k}`);
+        if (config.whitelist === 'error') {
+          const diffKeys = diff.map((k) => `${baseKey}${k}`)
           const error = Error(
-            `[${type}] Not permitted keys: ${diffKeys.join(", ")}`
-          );
+            `[${type}] Not permitted keys: ${diffKeys.join(', ')}`
+          )
           if (config.onError) {
-            const res = config.onError(`${type}.whitelist`, baseKey, diffKeys);
-            if (res) throw new HttpError(res);
+            const res = config.onError(`${type}.whitelist`, baseKey, diffKeys)
+            if (res) throw new HttpError(res)
           }
-          throw error;
-        } else if (config.whitelist === "ignore")
-          for (const key of diff) delete params[key];
+          throw error
+        } else if (config.whitelist === 'ignore')
+          for (const key of diff) delete params[key]
     }
     for (const key in config.rules) {
-      const rule = config.rules[key];
+      const rule = config.rules[key]
 
-      if (!rule) continue;
+      if (!rule) continue
 
-      let value = params[key];
+      let value = params[key]
 
       // default
       if (rule.default)
-        if (type === "cookie" || type === "session")
-          this.logger.warn("Cookie and Session not support default rule.");
-        else if (typeof value === "undefined" && rule.default) {
+        if (type === 'cookie' || type === 'session')
+          this.logger.warn('Cookie and Session not support default rule.')
+        else if (typeof value === 'undefined' && rule.default) {
           value =
-            typeof rule.default === "function"
+            typeof rule.default === 'function'
               ? rule.default(this.request)
-              : rule.default;
-          params[key] = value;
+              : rule.default
+          params[key] = value
         }
 
       // required
       if (rule.required)
-        if (typeof value === "undefined" || value === null) {
-          const error = Error(`[${type}] ${baseKey}${key} is required.`);
+        if (typeof value === 'undefined' || value === null) {
+          const error = Error(`[${type}] ${baseKey}${key} is required.`)
           if (config.onError) {
             const res = config.onError(
               `${type}.rule.required`,
               `${baseKey}${key}`,
               value
-            );
-            if (res) throw new HttpError(res);
+            )
+            if (res) throw new HttpError(res)
           }
-          throw error;
+          throw error
         }
 
-      if (typeof value !== "undefined" && value !== null) {
+      if (typeof value !== 'undefined' && value !== null) {
         // type
         if (rule.type)
-          if (type === "cookie")
-            this.logger.warn("Cookie not support type rule");
+          if (type === 'cookie')
+            this.logger.warn('Cookie not support type rule')
           else {
-            let typed = true;
+            let typed = true
             switch (rule.type) {
-              case "array":
-                typed = Array.isArray(value);
-                break;
-              case "object":
+              case 'array':
+                typed = Array.isArray(value)
+                break
+              case 'object':
                 typed =
-                  Object.prototype.toString.call(value) === "[object Object]";
-                break;
+                  Object.prototype.toString.call(value) === '[object Object]'
+                break
               default:
-                typed = typeof value === rule.type;
-                break;
+                typed = typeof value === rule.type
+                break
             }
 
             if (!typed) {
               const error = Error(
                 `[${type}] ${baseKey}${key} must be a ${rule.type}.`
-              );
+              )
               if (config.onError) {
                 const res = config.onError(
                   `${type}.rule.type`,
                   `${baseKey}${key}`,
                   value
-                );
-                if (res) throw new HttpError(res);
+                )
+                if (res) throw new HttpError(res)
               }
-              throw error;
+              throw error
             }
           }
 
         // regexp
         if (
           rule.regexp &&
-          (rule.type === "string" || !rule.type) &&
+          (rule.type === 'string' || !rule.type) &&
           !rule.regexp.test(value)
         ) {
           const error = Error(
             `[${type}] ${baseKey}${key} must match ${rule.regexp}.`
-          );
-          console.log("--------->22222");
+          )
+          console.log('--------->22222')
           if (config.onError) {
             const res = config.onError(
               `${type}.rule.regexp`,
               `${baseKey}${key}`,
               value
-            );
-            if (res) throw new HttpError(res);
+            )
+            if (res) throw new HttpError(res)
           }
-          throw error;
+          throw error
         }
         // in
         if (rule.in && !rule.in.includes(value)) {
           const error = Error(
-            `[${type}] ${baseKey}${key} must be in ${rule.in.join(", ")}.`
-          );
+            `[${type}] ${baseKey}${key} must be in ${rule.in.join(', ')}.`
+          )
           if (config.onError) {
             const res = config.onError(
               `${type}.rule.in`,
               `${baseKey}${key}`,
               value
-            );
-            if (res) throw new HttpError(res);
+            )
+            if (res) throw new HttpError(res)
           }
-          throw error;
+          throw error
         }
 
         // nest config
         if (rule.config)
-          if (type === "cookie")
-            this.logger.warn("Cookie not support nest rule.");
+          if (type === 'cookie')
+            this.logger.warn('Cookie not support nest rule.')
           else if (Array.isArray(value))
-            // array
+          // array
 
             for (const val of value)
               this.validContent(
@@ -280,15 +280,15 @@ export class Validator<
                 val,
                 baseKey ? `${baseKey}.${key}.` : `${key}.`,
                 rule.config as ValidatorOptions
-              );
-          else if (typeof value === "object")
+              )
+          else if (typeof value === 'object')
             // object
             this.validContent(
               type,
               value,
               baseKey ? `${baseKey}.${key}.` : `${key}.`,
               rule.config as ValidatorOptions
-            );
+            )
       }
     }
   }
