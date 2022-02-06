b562290af633fa7d892969841ce8cc593c86205e
Ben <benz303bb@gmail.com>
2022-02-06 16:43:17 +0800

    merge

--- packages/deep_merge/src/index.ts
+++ packages/deep_merge/src/index.ts
@@ -4,13 +4,17 @@ const shouldMerge = function (item: any) {
 }
 
 /**
- * 合并对象
- * @description
- * 注意事项：
- * * 合并时会复制对象，不会修改原对象
- * * 合并顺序是后面的覆盖前面的
- * * 若有数组形式的属性，数组里的内容将被去重合并
- * @param sources [...any] 合并对象
+ * Deep merge two objects or arrays.
+ *
+ * Features:
+ * * All objects will be cloned before merging.
+ * * Merging order is from right to left.
+ * * If an array include same objects, it will be unique to one.
+ *
+ * ```ts
+ * deepMerge({ a: 1 }, { a: 2 }) // { a: 2 }
+ * deepMerge([1, 2], [2, 3]) // [1, 2, 3]
+ * ```
  */
 export function deepMerge (...sources: any[]): any {
   let acc = Object.create(null)
