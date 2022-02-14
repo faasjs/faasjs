5cb34dad90fd14a49519a8ca67c45969fae12e70
Ben <benz303bb@gmail.com>
2022-02-14 14:23:07 +0800

    fix load path

--- packages/ant-design/src/Calendar.tsx
+++ packages/ant-design/src/Calendar.tsx
@@ -1,8 +1,8 @@
 import type { Dayjs } from 'dayjs'
 import type { CalendarProps as AntdProps } from 'antd'
 
-import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs'
-import generateCalendar from 'antd/es/calendar/generateCalendar'
+import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs.js'
+import generateCalendar from 'antd/es/calendar/generateCalendar.js'
 
 import 'antd/es/calendar/style'
 
--- packages/ant-design/src/DatePicker.tsx
+++ packages/ant-design/src/DatePicker.tsx
@@ -1,10 +1,10 @@
 import type { Dayjs } from 'dayjs'
 import type { PickerDateProps } from 'antd/es/date-picker/generatePicker'
 
-import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs'
-import generatePicker from 'antd/es/date-picker/generatePicker'
+import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs.js'
+import generatePicker from 'antd/es/date-picker/generatePicker/index.js'
 
-import 'antd/es/date-picker/style'
+import 'antd/es/date-picker/style/index.js'
 
 export type DatePickerProps = PickerDateProps<Dayjs>
 export const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig)
