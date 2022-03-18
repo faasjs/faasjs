13c4bca9474e02e507dd3af03a8e3328300fd50f
Ben <ben@zfben.com>
2022-03-18 12:36:12 +0800

    fix case

--- packages/ts-transform/src/__tests__/bundle.test.ts
+++ packages/ts-transform/src/__tests__/bundle.test.ts
@@ -10,14 +10,6 @@ describe('bundle', () => {
   it('require file', async () => {
     const res = await bundle({ filename: require.resolve('./require.ts') })
 
-    console.log(res.code)
-
-    expect(res.code).toContain(`var load = __swcpack_require__.bind(void 0, function(module, exports) {
-    module.exports = 'required';
-});
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const required = load();
-export { required as default };
-`)
+    expect(res.code).toContain('function __swcpack_require__(mod) {')
   })
 })
