6c7c1168cceae55fd8a1d7bc8b42eecad58f89ee
Ben <ben@zfben.com>
2022-04-27 13:11:27 +0800

    update jest

--- packages/jest/index.js
+++ packages/jest/index.js
@@ -4,41 +4,37 @@ const { extname } = require('path')
 
 const skipTypes = /(css|less|sass|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)/
 
+const transformer = createTransformer({
+  jsc: {
+    parser: {
+      syntax: 'typescript',
+      tsx: true,
+    },
+    target: 'es2019',
+    transform: { react: { runtime: 'automatic' } },
+  }
+})
+
 module.exports = {
-  createTransformer () {
-    const transformer = createTransformer({
-      jsc: {
-        parser: {
-          syntax: 'typescript',
-          tsx: true,
-        },
-        target: 'es2019',
-        transform: { react: { runtime: 'automatic' } },
-      }
-    })
+  process (src, filename, options) {
+    if (filename.endsWith('.js'))
+      return src
 
-    return {
-      process (src, filename, options) {
-        if (filename.endsWith('.js'))
-          return src
+    if (skipTypes.test(extname(filename)))
+      return ''
 
-        if (skipTypes.test(extname(filename)))
-          return ''
+    return transformer.process(src, filename, options)
+  },
+  processAsync(src, filename, options) {
+    if (filename.endsWith('.js'))
+      return Promise.resolve(src)
 
-        return transformer.process(src, filename, options)
-      },
-      processAsync(src, filename, options) {
-        if (filename.endsWith('.js'))
-          return Promise.resolve(src)
+    if (skipTypes.test(extname(filename)))
+      return Promise.resolve('')
 
-        if (skipTypes.test(extname(filename)))
-          return Promise.resolve('')
-
-        return transformer.processAsync(src, filename, options)
-      },
-      getCacheKey (src, filename, ...rest) {
-        return transformer.getCacheKey(src, filename, ...rest)
-      }
-    }
+    return transformer.processAsync(src, filename, options)
+  },
+  getCacheKey (src, filename, ...rest) {
+    return transformer.getCacheKey(src, filename, ...rest)
   }
 }
