ee132e986fa04b8fc7876fdebc663a46f2103846
Ben <ben@zfben.com>
2022-08-05 16:49:58 +0800

    add more cases

---
+++ packages/ts-transform/src/__tests__/transform.test.ts
@@ -0,0 +1,7 @@
+import { transform } from '..'
+
+describe('transform', () => {
+  it('should work', async () => {
+    expect(transform('const a: string = \'a\'')).toEqual({ code: '"use strict";\nconst a = \'a\';\n' })
+  })
+})
