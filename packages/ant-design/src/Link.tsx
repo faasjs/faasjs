d20cd9570b7ae82621e2b6e8824b272eb5a5373c
Ben <ben@zfben.com>
2022-09-16 16:41:08 +0800

    add button to link

--- packages/ant-design/src/Link.tsx
+++ packages/ant-design/src/Link.tsx
@@ -1,19 +1,33 @@
 import type { ReactNode, CSSProperties } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { useConfigContext } from './Config'
+import { Button, ButtonProps } from 'antd'
 
 export function Link ({
-  href, target, text, children, style
+  href, target, text, children, style, button,
 }: {
   href: string
   target?: string
   text?: string | number
   children?: ReactNode
   style?: CSSProperties
+  button?:ButtonProps
 }) {
   const { Link } = useConfigContext()
 
   style = Object.assign({ cursor: 'pointer' }, style)
+
+  if (button)
+    return <Button { ...button }>
+      <RouterLink
+        to={ href }
+        target={ target || Link?.target }
+        style={ {
+          ...Link.style,
+          ...style || {},
+        } }
+      >{text || children}</RouterLink>
+    </Button>
 
   return <RouterLink
     to={ href }
