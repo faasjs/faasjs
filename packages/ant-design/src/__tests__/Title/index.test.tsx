6eed66bf3c1ec3fd133285c0d2c668867e887c41
Ben <ben@zfben.com>
2022-11-19 10:43:15 +0800

    fix cases

--- packages/ant-design/src/__tests__/Title/index.test.tsx
+++ packages/ant-design/src/__tests__/Title/index.test.tsx
@@ -63,28 +63,6 @@ describe('Title', () => {
     })
   })
 
-  describe('should return a PageHeader', () => {
-    it('when title is a text', () => {
-      const { container } = render(<Title
-        title='title'
-        header={ { subTitle: 'subTitle' } }
-      />)
-
-      expect(document.title).toEqual('title')
-      expect(container.innerHTML).toEqual('<div class="ant-page-header ant-page-header-ghost"><div class="ant-page-header-heading"><div class="ant-page-header-heading-left"><span class="ant-page-header-heading-title" title="title">title</span><span class="ant-page-header-heading-sub-title" title="subTitle">subTitle</span></div></div></div>')
-    })
-
-    it('when title is an array', () => {
-      const { container } = render(<Title
-        title={ ['a', 'b'] }
-        header={ { subTitle: 'subTitle' } }
-      />)
-
-      expect(document.title).toEqual('a - b')
-      expect(container.innerHTML).toEqual('<div class="ant-page-header ant-page-header-ghost"><div class="ant-page-header-heading"><div class="ant-page-header-heading-left"><span class="ant-page-header-heading-title" title="a">a</span><span class="ant-page-header-heading-sub-title" title="subTitle">subTitle</span></div></div></div>')
-    })
-  })
-
   describe('it should return children', () => {
     it('static children', () => {
       const { container } = render(<Title title='title'><h2>h2</h2></Title>)
