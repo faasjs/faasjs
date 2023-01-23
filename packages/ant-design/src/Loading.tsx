d02584a30b1b6513c14e5fd78e6e4385ac10ff2e
Ben <ben@zfben.com>
2023-01-23 12:49:58 +0800

    add children to Loading

--- packages/ant-design/src/Loading.tsx
+++ packages/ant-design/src/Loading.tsx
@@ -2,11 +2,24 @@ import { Spin } from 'antd'
 
 /**
  * Loading component based on Spin
+ *
+ * ```tsx
+ * <Loading /> // display loading
+ *
+ * <Loading loading={ !remoteData }>
+ *  <div>{remoteData}</div>
+ * </Loading>
+ * ```
  */
 export function Loading (props: {
   style?: React.CSSProperties
   size?: 'small' | 'default' | 'large'
+  loading?: boolean
+  children?: React.ReactNode
 }) {
+  if (props.loading === false)
+    return <>{props.children}</>
+
   return <div style={ {
     ...props.style || {},
     ...(!props.size || props.size === 'large' ? {
