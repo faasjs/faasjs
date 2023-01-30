8d8b584bc3594d6f8b90b27b6b9650094ba93fc8
Ben <ben@zfben.com>
2023-01-30 12:53:36 +0800

    fix deps version

--- build.js
+++ build.js
@@ -19,12 +19,6 @@ async function build(path, dts = false) {
         pkg.dependencies[name] = '^' + version
     }
   }
-  if (pkg.peerDependencies) {
-    for (const name of Object.keys(pkg.peerDependencies)) {
-      if (name.startsWith('@faasjs/'))
-        pkg.peerDependencies[name] = '^' + version
-    }
-  }
   if (pkg.devDependencies) {
     for (const name of Object.keys(pkg.devDependencies)) {
       if (name.startsWith('@faasjs/'))
