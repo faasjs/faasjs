a75c250a7d22b408e97366c7ad25a223a673bd93
Ben <ben@zfben.com>
2023-02-12 21:58:05 +0800

    extend App

--- packages/ant-design/src/App.tsx
+++ packages/ant-design/src/App.tsx
@@ -1,14 +1,28 @@
-import { App as AntdApp } from 'antd'
+import { App as AntdApp, ConfigProvider } from 'antd'
 import type { AppProps as AntdProps } from 'antd/es/app'
+import type { ConfigProviderProps } from 'antd/es/config-provider'
+import { StyleProvider, legacyLogicalPropertiesTransformer } from '@ant-design/cssinjs'
+import type { StyleProviderProps } from '@ant-design/cssinjs/lib/StyleContext'
 
 export interface AppProps extends AntdProps {
   children: React.ReactNode
+  styleProviderProps?: StyleProviderProps
+  configProviderProps?: ConfigProviderProps
 }
 
 export function App (props: AppProps) {
-  return <AntdApp { ...props }>
-    { props.children }
-  </AntdApp>
+  return <StyleProvider
+    { ...props.styleProviderProps }
+  >
+    <ConfigProvider { ...Object.assign<StyleProviderProps, StyleProviderProps>(props.configProviderProps, {
+      hashPriority: 'high',
+      transformers: [legacyLogicalPropertiesTransformer],
+    }) }>
+      <AntdApp { ...props }>
+        { props.children }
+      </AntdApp>
+    </ConfigProvider>
+  </StyleProvider>
 }
 
 App.useApp = AntdApp.useApp
