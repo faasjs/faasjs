42603ff5ff38280469064408399bc5bf1cd44e7b
Ben <ben@zfben.com>
2023-12-05 19:42:51 +0800

    add useSignalState

--- packages/react/src/__tests__/signal.test.tsx
+++ packages/react/src/__tests__/signal.test.tsx
@@ -1,11 +1,16 @@
-import { signal } from '../signal'
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { signal, useSignalState } from '../signal'
 
 describe('signal', () => {
   it('should work with debug', () => {
     const logs: any[] = []
     console.debug = jest.fn().mockImplementation((...args) => logs.push(args))
 
-    const state = signal(0, { debugName: 'test' })
+    const state = signal(0, { debugName: 'signal' })
 
     expect(state.value).toBe(0)
 
@@ -14,8 +19,38 @@ describe('signal', () => {
     expect(state.value).toBe(1)
 
     expect(logs).toEqual([
-      ['test', 0],
-      ['test', 1],
+      ['signal', 0],
+      ['signal', 1],
     ])
+  })
+})
+
+describe('useSignalState', () => {
+  test('should work', async () => {
+    function Test() {
+      const [state, setState] = useSignalState(0, {
+        debugName: 'useSignalState',
+      })
+
+      return (
+        <div>
+          {state}
+          <button type='button' onClick={() => setState(1)} />
+          <button type='button' onClick={() => setState(prev => prev + 2)} />
+        </div>
+      )
+    }
+
+    render(<Test />)
+
+    expect(screen.getByText('0')).toBeInTheDocument()
+
+    await userEvent.click(screen.getAllByRole('button')[0])
+
+    expect(await screen.findByText('1')).toBeInTheDocument()
+
+    await userEvent.click(screen.getAllByRole('button')[1])
+
+    expect(await screen.findByText('3')).toBeInTheDocument()
   })
 })
--- packages/react/src/signal.ts
+++ packages/react/src/signal.ts
@@ -4,17 +4,18 @@ import {
   computed,
   batch,
   useComputed,
-  useSignal,
+  useSignal as originUseSignal,
   useSignalEffect,
 } from '@preact/signals-react'
+import type { SetStateAction } from 'react'
 
 export {
   originSignal,
+  originUseSignal,
   effect,
   computed,
   batch,
   useComputed,
-  useSignal,
   useSignalEffect,
 }
 
@@ -48,4 +49,43 @@ export function signal<T = any>(
     effect(() => console.debug(options.debugName, state.value))
 
   return state
+}
+
+/**
+ * Create a [signal](https://preactjs.com/guide/v10/signals) like useState.
+ *
+ * ```tsx
+ * import { useSignalState, useSignalEffect } from '@faasjs/react'
+ *
+ * function App () {
+ *   const [count, setCount] = useSignalState(0, { debugName: 'count' })
+ *
+ *   useSignalEffect(() => console.log('count', count))
+ *
+ *   return <div>
+ *     <button onClick={() => setCount(count + 1)}>+</button>
+ *     <span>{count}</span>
+ *   </div>
+ * }
+ * ```
+ */
+export function useSignalState<T = any>(
+  initialValue: T,
+  options: SignalOptions = {}
+): readonly [T, (changes: SetStateAction<T>) => void] {
+  const state = originUseSignal<T>(initialValue)
+
+  if (options.debugName) {
+    useSignalEffect(() => console.log(options.debugName, state.value))
+  }
+
+  return [
+    state.value,
+    (changes: SetStateAction<T>) => {
+      state.value =
+        typeof changes === 'function'
+          ? (changes as (prevState: T) => T)(state.value)
+          : changes
+    },
+  ] as const
 }
