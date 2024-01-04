b26d8d166b8253c67d476274f3c2e125384bfb01
Ben <ben@zfben.com>
2024-01-04 09:24:53 +0800

    improve request log

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,5 +1,5 @@
-import * as http from 'node:http'
-import * as https from 'node:https'
+import http from 'node:http'
+import https from 'node:https'
 import { URL } from 'node:url'
 import { readFileSync, createWriteStream } from 'node:fs'
 import { basename } from 'node:path'
@@ -172,73 +172,57 @@ export class ResponseError extends Error {
  */
 export async function request<T = any>(
   url: string,
-  {
-    headers,
-    method,
-    query,
-    body,
-    timeout,
-    auth,
-    file,
-    downloadStream,
-    downloadFile,
-    pfx,
-    passphrase,
-    agent,
-    parse,
-    logger,
-  }: RequestOptions = { headers: {} }
+  options: RequestOptions = { headers: {} }
 ): Promise<Response<T>> {
-  if (!logger) logger = new Logger('request')
+  const logger = options.logger || new Logger('request')
 
   if (mock) {
-    logger.debug('mock %s %j', url, {
-      headers,
-      method,
-      query,
-      body,
-    })
-    return mock(url, {
-      headers,
-      method,
-      query,
-      body,
-    })
+    logger.debug('mock %s %j', url, options)
+    return mock(url, options)
   }
 
-  if (query) {
+  if (options.query) {
     if (!url.includes('?')) url += '?'
     else if (!url.endsWith('?')) url += '&'
 
-    url += querystringify(query)
+    url += querystringify(options.query)
   }
+
+  if (!options.headers) options.headers = {}
 
   const uri = new URL(url)
   const protocol = uri.protocol === 'https:' ? https : http
 
   if (!uri.protocol) throw Error('Unknown protocol')
 
-  const options: https.RequestOptions = {
+  const requestOptions: https.RequestOptions = {
     headers: {},
     host: uri.host ? uri.host.replace(/:[0-9]+$/, '') : uri.host,
-    method: method ? method.toUpperCase() : 'GET',
+    method: options.method ? options.method.toUpperCase() : 'GET',
     path: uri.pathname + uri.search,
     port: uri.port || (uri.protocol === 'https:' ? '443' : '80'),
-    timeout: timeout || 5000,
-    auth,
-    pfx,
-    passphrase,
-    agent,
+    timeout: options.timeout || 5000,
+    auth: options.auth,
+    pfx: options.pfx,
+    passphrase: options.passphrase,
+    agent: options.agent,
   }
 
-  if (!options.headers['Accept-Encoding'] && !downloadFile && !downloadStream) {
+  if (
+    !options.headers['Accept-Encoding'] &&
+    !options.downloadFile &&
+    !options.downloadStream
+  )
     options.headers['Accept-Encoding'] = 'br,gzip'
-  }
 
-  for (const key in headers)
-    if (typeof headers[key] !== 'undefined' && headers[key] !== null)
-      options.headers[key] = headers[key]
+  for (const key in options.headers)
+    if (
+      typeof options.headers[key] !== 'undefined' &&
+      options.headers[key] !== null
+    )
+      requestOptions.headers[key] = options.headers[key]
 
+  let body = options.body
   if (body && typeof body !== 'string')
     if (
       options.headers['Content-Type']
@@ -249,7 +233,7 @@ export async function request<T = any>(
     else body = JSON.stringify(body)
 
   if (body && !options.headers['Content-Length'])
-    options.headers['Content-Length'] = Buffer.byteLength(body as string)
+    requestOptions.headers['Content-Length'] = Buffer.byteLength(body as string)
 
   const requestId = randomUUID()
 
@@ -259,125 +243,134 @@ export async function request<T = any>(
       body,
     })
 
-    const req = protocol.request(options, (res: http.IncomingMessage) => {
-      if (downloadStream) {
-        res.pipe(downloadStream, { end: true })
-        downloadStream.on('finish', () => {
-          res.destroy()
-          downloadStream.end()
-          resolve(undefined)
-        })
-        return
-      }
-
-      if (downloadFile) {
-        logger.debug('downloadFile')
-        const stream = createWriteStream(downloadFile, { autoClose: true })
-        res.pipe(stream, { end: true })
-        res.on('end', () => {
-          logger.debug('end')
-          res.destroy()
-        })
-        stream.on('finish', () => {
-          stream.end()
-          stream.close()
-          logger.debug('finish')
-        })
-        stream.on('close', () => {
-          logger.debug(
-            'finish',
-            res.closed,
-            res.destroyed,
-            stream.destroyed,
-            stream.closed,
-            req.closed,
-            req.destroyed
-          )
-          resolve(undefined)
-        })
-        return
-      }
-
-      let stream: NodeJS.ReadableStream = res
-
-      switch (res.headers['content-encoding']) {
-        case 'br':
-          stream = res.pipe(createBrotliDecompress())
-          break
-        case 'gzip':
-          stream = res.pipe(createGunzip())
-          break
-      }
-
-      const raw: Buffer[] = []
-      stream.on('data', (chunk: any) => raw.push(chunk))
-      stream.on('end', () => {
-        const data = Buffer.concat(raw).toString()
-        logger.timeEnd(
-          requestId,
-          'response %s %s %s %j',
-          res.statusCode,
-          res.headers['content-type'],
-          res.headers['content-encoding'],
-          data
-        )
-
-        const response = Object.create(null)
-        response.request = options
-        response.request.body = body
-        response.statusCode = res.statusCode
-        response.statusMessage = res.statusMessage
-        response.headers = res.headers
-        response.body = data
-
-        if (
-          response.body &&
-          response.headers['content-type'] &&
-          response.headers['content-type'].includes('application/json')
-        )
-          try {
-            response.body = (parse || JSON.parse)(response.body)
-            logger.debug('response.parse JSON')
-          } catch (error: any) {
-            logger.warn('response plain body', response.body)
-            logger.error(error)
-          }
-
-        if (response.statusCode >= 200 && response.statusCode < 400)
-          resolve(response)
-        else {
-          logger.debug('response.error %j', response)
-          reject(
-            new ResponseError(
-              `${res.statusMessage || res.statusCode} ${options.host}${
-                options.path
-              }`,
-              response
-            )
-          )
+    const req = protocol.request(
+      requestOptions,
+      (res: http.IncomingMessage) => {
+        if (options.downloadStream) {
+          options.downloadStream
+            .on('error', (error: Error) => {
+              logger.timeEnd(requestId, 'response.error %j', error)
+              reject(error)
+            })
+            .on('finish', () => {
+              logger.timeEnd(
+                requestId,
+                'response %s %s',
+                res.statusCode,
+                res.headers['content-type']
+              )
+              options.downloadStream.end()
+              resolve(undefined)
+            })
+          res.pipe(options.downloadStream, { end: true })
+          return
         }
-      })
-      stream.on('error', (e: Error) => {
-        logger.timeEnd(requestId, 'response.error %j', e)
-        reject(e)
-      })
-    })
+
+        if (options.downloadFile) {
+          const stream = createWriteStream(options.downloadFile)
+            .on('error', (error: Error) => {
+              logger.timeEnd(requestId, 'response.error %j', error)
+              stream.destroy()
+              reject(error)
+            })
+            .on('finish', () => {
+              logger.timeEnd(
+                requestId,
+                'response %s %s %s',
+                res.statusCode,
+                res.headers['content-type'],
+                stream.bytesWritten
+              )
+              resolve(undefined)
+            })
+
+          res.pipe(stream, { end: true })
+
+          return
+        }
+
+        let stream: NodeJS.ReadableStream = res
+
+        switch (res.headers['content-encoding']) {
+          case 'br':
+            stream = res.pipe(createBrotliDecompress())
+            break
+          case 'gzip':
+            stream = res.pipe(createGunzip())
+            break
+        }
+
+        const raw: Buffer[] = []
+        stream
+          .on('error', (e: Error) => {
+            logger.timeEnd(requestId, 'response.error %j', e)
+            reject(e)
+          })
+          .on('end', () => {
+            const data = Buffer.concat(raw).toString()
+            logger.timeEnd(
+              requestId,
+              'response %s %s %s %j',
+              res.statusCode,
+              res.headers['content-type'],
+              res.headers['content-encoding'],
+              data
+            )
+
+            const response = Object.create(null)
+            response.request = requestOptions
+            response.request.body = body
+            response.statusCode = res.statusCode
+            response.statusMessage = res.statusMessage
+            response.headers = res.headers
+            response.body = data
+
+            if (
+              response.body &&
+              response.headers['content-type'] &&
+              response.headers['content-type'].includes('application/json')
+            )
+              try {
+                response.body = (options.parse || JSON.parse)(response.body)
+                logger.debug('response.parse JSON')
+              } catch (error: any) {
+                logger.warn('response plain body', response.body)
+                logger.error(error)
+              }
+
+            if (response.statusCode >= 200 && response.statusCode < 400)
+              resolve(response)
+            else {
+              logger.debug('response.error %j', response)
+              reject(
+                new ResponseError(
+                  `${res.statusMessage || res.statusCode} ${
+                    requestOptions.host
+                  }${requestOptions.path}`,
+                  response
+                )
+              )
+            }
+          })
+          .on('data', (chunk: any) => raw.push(chunk))
+      }
+    )
 
     if (body) req.write(body)
 
-    if (file) {
+    if (options.file) {
       const crlf = '\r\n'
       const boundary = `--${Math.random().toString(16)}`
       const delimiter = `${crlf}--${boundary}`
       const headers = [
         `Content-Disposition: form-data; name="file"; filename="${basename(
-          file
+          options.file
         )}"${crlf}`,
       ]
 
       const multipartBody = Buffer.concat([
         Buffer.from(delimiter + crlf + headers.join('') + crlf),
-        readFileSync(file),
+        readFileSync(options.file),
         Buffer.from(`${delimiter}--`),
       ])
 
