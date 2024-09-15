ab268f63e8fab5b3570095ac601a41aeb8148777
Ben <ben@zfben.com>
2024-09-15 18:21:46 +0700

    update doc

--- docs/prepare.ts
+++ docs/prepare.ts
@@ -1,16 +1,15 @@
 import { execSync} from 'node:child_process'
-import {dirname} from 'node:path'
+import { dirname } from 'node:path'
 import { readFileSync, writeFileSync } from 'node:fs'
 import { Glob } from "bun"
 
-function run(cmd) {
+function run(cmd: string) {
   console.log(cmd)
   execSync(cmd, { stdio: 'inherit' })
 }
 
 const packages = new Glob('../packages/**/*.md').scanSync()
 
-console.log(packages)
 for (const file of packages) {
   const target = file.replace('../packages/', './doc/')
   run(`mkdir -p ${dirname(target)} &`)
@@ -45,8 +44,6 @@ for (const file of files) {
 
 const images = new Glob('../images/**/*.md').scanSync()
 
-console.log(images)
-
 for (const file of images) {
   const target = file.replace('../images/', './doc/images/')
   run(`mkdir -p ${dirname(target)} &`)
@@ -62,23 +59,7 @@ writeFileSync(
 
 const roots = new Glob('../*.md').scanSync()
 
-console.log(roots)
-
 for (const file of roots) {
   if (file === '../README.md') continue
   run(`cp ${file} ${file.replace('../', './')}`)
 }
-
-// Fix modules' links
-// for (const file of [
-//   './doc/react/interfaces/FaasDataWrapperProps.md',
-//   './doc/react/interfaces/FaasReactClientInstance.md',
-//   './doc/vue-plugin/interfaces/FaasVuePluginOptions.md'
-// ]) {
-//   writeFileSync(file, readFileSync(file, 'utf-8').toString()
-//   .replaceAll('../modules.md#faasaction', '../type-aliases/FaasAction.md')
-//   .replaceAll('../modules.md#faasparams', '../type-aliases/FaasParams.md')
-//   .replaceAll('../modules.md#faasdata', '../type-aliases/FaasData.md')
-//   .replaceAll('../modules.md#options', '../type-aliases/Options.md')
-// )
-// }
