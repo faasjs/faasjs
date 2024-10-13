1fc65a6974d124ecde25bb8d989bac16ed2e3da3
Ben <ben@zfben.com>
2024-10-13 15:08:36 +0700

    update doc

--- packages/browser/src/generateId.ts
+++ packages/browser/src/generateId.ts
@@ -1,7 +1,14 @@
 /**
- * Generate random id
+ * Generate random id with prefix
+ *
+ * Note: The random part is **18 characters long**.
  *
  * @param prefix prefix of id
+ *
+ * @example
+ * ```ts
+ * generateId('prefix-') // prefix-1z3b4c5d6e
+ * ```
  */
 export function generateId(prefix?: string) {
   return `${prefix || ''}${Date.now().toString(36).padStart(8, '0')}${Math.random()
