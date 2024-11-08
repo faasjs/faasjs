68fb53f50b514205779a3dcf970168b29dabb4e4
Ben <ben@zfben.com>
2024-11-08 17:43:31 +0700

    add usePrevious

--- packages/react/src/equal.ts
+++ packages/react/src/equal.ts
@@ -130,3 +130,20 @@ export function useEqualCallback<T extends (...args: any[]) => any>(
     useEqualMemoize(dependencies)
   )
 }
+
+/**
+ * Hook to store the previous value of a state or prop.
+ *
+ * @template T - The type of the value.
+ * @param {T} value - The current value to be stored.
+ * @returns {T | undefined} - The previous value, or undefined if there is no previous value.
+ */
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>()
+
+  useEffect(() => {
+    ref.current = value
+  })
+
+  return ref.current
+}
