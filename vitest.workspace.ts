03dfb0e874cf57377907e8574939e405b0740f5b
Ben <ben@zfben.com>
2024-12-28 11:19:04 +0700

    add vitest

---
+++ vitest.workspace.ts
@@ -0,0 +1,11 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  {
+    test: {
+      globals: true,
+      include: ['packages/**/*.test.ts'],
+      environment: 'node',
+    }
+  }
+])
