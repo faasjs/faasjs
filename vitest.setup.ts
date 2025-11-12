2d8232cfe35e22e41d0bcdef3804279652173d21
Ben <ben@zfben.com>
2025-11-12 19:13:49 +0800

    fix case

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,7 @@
+import { GlobalRegistrator } from '@happy-dom/global-registrator'
 import { beforeEach } from 'vitest'
+
+GlobalRegistrator.register()
 
 beforeEach(() => {
   if (typeof document === 'undefined') return
